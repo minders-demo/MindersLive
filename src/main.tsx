31936cd9785d8201e4108fab12ca06d1af5cbeec
minders-demo <sergio.gomez@minders.io>
2026-09-15 21:01:58 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import {initializeFeatureExperiment} from './lib/amplitude';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function bootstrap() {
+  await initializeFeatureExperiment();
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
+
+void bootstrap();
